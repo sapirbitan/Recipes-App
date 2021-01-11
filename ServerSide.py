1f6bcb410b757ef7ecf3776087b14b2c7ed8be24
sapirbitan <32384529+sapirbitan@users.noreply.github.com>
2021-01-11 21:30:21 +0200

    First attempt at creating a server
    First attempt at creating a server

--- ServerSide.py
+++ ServerSide.py
@@ -1,1 +1,12 @@
-
+import socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("0.0.0.0", 8820))
+server_socket.listen()
+print("Server is up and running")
+(client_socket, client_adress)  =server_socket.accept()
+print("Client connected")
+data = client_socket.recv(1024).decode()
+print("Client sent: " + data)
+client_socket.send(data.encode())
+client_socket.close()
+server_socket.close()
